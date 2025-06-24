7422ac3a5a60f533537e20c2a16106ca77c06bc6
Julian <48520440@est.ort.edu.ar>
2025-06-24 10:42:42 -0300

    avances

--- src/components/CardProducto.jsx
+++ src/components/CardProducto.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import './CardProducto.css'; 
+import './CardProducto.css';
 
 export default function CardProducto({ producto }) {
   return (
-    <a href={`/producto/${producto.id}`} className="card">
+    <a href={`/TP7EFSI_Berman_Jolodovsky_Goldberg/producto/${producto.id}`} className="card">
       <img src={producto.images[0]} alt={producto.title} />
       <h2>{producto.title || 'Producto'}</h2>
       <h3>{`$${producto.price}`}</h3>
